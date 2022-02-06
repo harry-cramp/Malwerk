36bdb7da997f564ba51d7f7b696678885c459fc8
Harry Cramp <harry.cramp@student.manchester.ac.uk>
2022-02-06 15:23:20 +0000

    Alter apktool launcher to use local folders

--- run-apktool.sh
+++ run-apktool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf apk-extracted
-python3 ./apktool/apktool.py --apktool ./apktool/apktool_2.6.0.jar $1 >> $2/$3.txt
+python3 ./apktool/apktool.py --apktool ./apktool/apktool_2.6.0.jar $1 >> ./analysis_output/general/$2.txt
