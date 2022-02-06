75d4b5fc347b4ddbb67110e194a152c89413511e
Harry Cramp <harry.cramp@student.manchester.ac.uk>
2022-02-06 16:08:57 +0000

    Add feature extraction to apktool launcher

--- run-apktool.sh
+++ run-apktool.sh
@@ -2,3 +2,4 @@
 
 rm -rf apk-extracted
 python3 ./apktool/apktool.py --apktool ./apktool/apktool_2.6.0.jar $1 >> ./analysis_output/general/$2.txt
+python3 feature-extractor.py ./analysis_output/general/$2.txt
