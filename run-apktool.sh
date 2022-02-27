c030e10644ef7d0c3d113b3533011852619d05fc
Harry Cramp <harry.cramp@student.manchester.ac.uk>
2022-02-27 21:26:23 +0000

    Change relative file paths

--- run-apktool.sh
+++ run-apktool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf apk-extracted
-python3 ./apktool/apktool.py --apktool ./apktool/apktool_2.6.0.jar $1 >> ./analysis_output/general/$2.txt
-python3 feature-extractor.py ./analysis_output/general/$2.txt
+python3 ./Malwerk/apktool/apktool.py --apktool ./Malwerk/apktool/apktool_2.6.0.jar $1 >> ./Malwerk/analysis_output/general/$2.txt
+python3 ./Malwerk/feature-extractor.py ./Malwerk/analysis_output/general/$2.txt
